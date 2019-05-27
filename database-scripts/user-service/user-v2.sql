48cb33fb48002a2f55532d5579a8eb5beac14f42
shubhamknoldus <shubham.verma@knoldus.in>
2019-05-27 16:49:15 +0530

    1) Moved all db script to a single folder 2) Made isActive for user as 0 as a defult value

--- database-scripts/user-service/user-v2.sql
+++ database-scripts/user-service/user-v2.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
 --
--- Host: localhost    Database: wallet
+-- Host: localhost    Database: users
 -- ------------------------------------------------------
 -- Server version	5.7.26-0ubuntu0.18.04.1
 
@@ -14,98 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `wallet`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `wallet` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `wallet`;
-
---
--- Table structure for table `wallet`
---
-
-DROP TABLE IF EXISTS `wallet`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wallet` (
-  `id` varchar(36) NOT NULL,
-  `userId` varchar(36) NOT NULL,
-  `amount` varchar(32) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `wallet`
---
-
-LOCK TABLES `wallet` WRITE;
-/*!40000 ALTER TABLE `wallet` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wallet` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `wallet_recharge_request`
---
-
-DROP TABLE IF EXISTS `wallet_recharge_request`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wallet_recharge_request` (
-  `id` varchar(36) NOT NULL,
-  `walletId` varchar(36) NOT NULL,
-  `requesterId` varchar(36) NOT NULL,
-  `issuerId` varchar(36) NOT NULL,
-  `requestedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `approvedOn` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `quantity` int(11) NOT NULL,
-  `status` varchar(36) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_wallet_rech` (`walletId`),
-  CONSTRAINT `fk_wallet_rech` FOREIGN KEY (`walletId`) REFERENCES `wallet` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `wallet_recharge_request`
---
-
-LOCK TABLES `wallet_recharge_request` WRITE;
-/*!40000 ALTER TABLE `wallet_recharge_request` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wallet_recharge_request` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `wallet_transactions`
---
-
-DROP TABLE IF EXISTS `wallet_transactions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wallet_transactions` (
-  `id` varchar(36) NOT NULL,
-  `userId` varchar(36) NOT NULL,
-  `walletId` varchar(36) NOT NULL,
-  `createdOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `description` varchar(200) NOT NULL,
-  `amount` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_wallet_tran` (`walletId`),
-  CONSTRAINT `fk_wallet_tran` FOREIGN KEY (`walletId`) REFERENCES `wallet` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `wallet_transactions`
---
-
-LOCK TABLES `wallet_transactions` WRITE;
-/*!40000 ALTER TABLE `wallet_transactions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wallet_transactions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Current Database: `users`
@@ -162,7 +70,7 @@ CREATE TABLE `users` (
   `role` varchar(10) NOT NULL,
   `password` varchar(50) NOT NULL,
   `empId` varchar(10) NOT NULL,
-  `isActive` tinyint(1) DEFAULT '1',
+  `isActive` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -186,4 +94,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-21 23:33:25
+-- Dump completed on 2019-05-27 16:44:35
