a1e6f4fd1ac5a6b0c10ce2b8641703b762bc85bc
B Sharma <BSharma@rccl.com>
2019-06-06 11:34:57 +0530

    Refactored test cases

--- walletservice/src/main/java/com/knoldus/wallet/controller/WalletController.java
+++ walletservice/src/main/java/com/knoldus/wallet/controller/WalletController.java
@@ -4,8 +4,10 @@ import com.knoldus.wallet.model.ResponseBody;
 import com.knoldus.wallet.model.wallet.RechargeRequest;
 import com.knoldus.wallet.model.wallet.RechargeResponse;
 import com.knoldus.wallet.service.WalletService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -22,7 +24,8 @@ public class WalletController {
         this.walletService = walletService;
     }
 
-    @PostMapping("/wallet")
+    @PostMapping(value = "/wallet", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
     Mono<ResponseBody<RechargeResponse>> rechargeWallet(@Valid @RequestBody RechargeRequest request) {
 
         return walletService.rechargeWallet(request);
