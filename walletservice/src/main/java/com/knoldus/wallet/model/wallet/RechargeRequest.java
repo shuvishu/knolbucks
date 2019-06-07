06c3ba8226b3047f17eaed0250c12e045c004e58
B Sharma <BSharma@rccl.com>
2019-06-07 11:50:58 +0530

    added @Min annotation in POJO

--- walletservice/src/main/java/com/knoldus/wallet/model/wallet/RechargeRequest.java
+++ walletservice/src/main/java/com/knoldus/wallet/model/wallet/RechargeRequest.java
@@ -2,8 +2,10 @@ package com.knoldus.wallet.model.wallet;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -14,6 +16,7 @@ public class RechargeRequest {
 
     @NotNull(message = "The quantity cannot be null")
     @Max(value = 5, message = "The max value of quantity should be less than 5")
+    @Min(value = 1, message = "The minimum value of quantity should be more than 0")
     Integer quantity;
 
     @NotBlank(message = "The employee Id cannot be blank")
